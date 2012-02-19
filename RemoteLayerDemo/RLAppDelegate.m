0cc825e7ad9841fde47d689ac0f55623e0537d57
Kurt Revis <krevis@snoize.com>
2012-02-19 02:53:21 -0800

    Whitespace and tabs

--- RemoteLayerDemo/RLAppDelegate.m
+++ RemoteLayerDemo/RLAppDelegate.m
@@ -30,9 +30,9 @@
 {
     if (_serviceConnection)
         xpc_release(_serviceConnection);
-    
+
     [_remoteLayer release];
-    
+
     [super dealloc];
 }
 
@@ -48,7 +48,7 @@
                 _serviceConnection = NULL;
             }
         });
-        
+
         xpc_connection_resume(_serviceConnection);
     }
 
@@ -63,35 +63,35 @@
     } else {
         NSLog(@"port published: %d", layerServer.serverPort);
     }
-    
+
     // Set up a view with a layer in it. We'll add the remote layer to it
     // as a sublayer.
-    
+
     CALayer* layer = [CALayer layer];
     layer.backgroundColor = CGColorCreateGenericRGB(0.f, 0.f, 1.f, 1.f);
-    
+
     _view.layer = layer;
     [_view setWantsLayer:YES];
 }
 
 - (IBAction)getRemoteLayer:(id)sender
-{   
+{
     // Send a message to the service, asking for a remote layer's client ID.
     // When it replies, make a CALayer with that client ID and add it to our view.
-    
+
     if (!self.serviceConnection)
         return;
-    
+
     xpc_object_t message = xpc_dictionary_create(NULL, NULL, 0);
     xpc_dictionary_set_uint64(message, "command", 1);
-    
-    xpc_connection_send_message_with_reply(self.serviceConnection, message, dispatch_get_main_queue(), ^(xpc_object_t reply) {        
+
+    xpc_connection_send_message_with_reply(self.serviceConnection, message, dispatch_get_main_queue(), ^(xpc_object_t reply) {
 #if 0
         char* s = xpc_copy_description(reply);
         NSLog(@"Got a reply: %s", s);
         free(s);
 #endif
-        
+
         if (xpc_get_type(reply) == XPC_TYPE_DICTIONARY) {
             uint32_t clientID = xpc_dictionary_get_uint64(reply, "clientID");
             if (clientID != 0) {
@@ -101,9 +101,9 @@
                     // And put it in our layer tree, making sure it doesn't animate
                     [CATransaction begin];
                     [CATransaction setDisableActions:YES];
-                    
+
                     _view.layer.sublayers = [NSArray arrayWithObject:remoteLayer];
-                    
+
                     // Center it in our bounds
                     CGRect b = _view.layer.bounds;
                     remoteLayer.position = CGPointMake(CGRectGetMidX(b), CGRectGetMidY(b));;
@@ -124,18 +124,18 @@
 }
 
 - (IBAction)changeColor:(id)sender
-{   
+{
     // Send a message to the service, asking it to change the remote layer's color.
     // We do not expect a reply.
-    
+
     if (!self.serviceConnection)
         return;
-    
+
     xpc_object_t message = xpc_dictionary_create(NULL, NULL, 0);
     xpc_dictionary_set_uint64(message, "command", 2);
-        
+
     xpc_connection_send_message(self.serviceConnection, message);
-    
+
     xpc_release(message);
 }
 
@@ -143,18 +143,18 @@
 {
     // Remove the remote layer.
     [CATransaction begin];
-    [CATransaction setDisableActions:YES];    
+    [CATransaction setDisableActions:YES];
     _view.layer.sublayers = [NSArray array];
     [CATransaction commit];
-    
+
     // And tell the service that it should tear itself down.
     if (self.serviceConnection)
     {
         xpc_object_t message = xpc_dictionary_create(NULL, NULL, 0);
         xpc_dictionary_set_uint64(message, "command", 3);
-        
+
         xpc_connection_send_message(self.serviceConnection, message);
-        
+
         xpc_release(message);
     }
 }
--- RemoteLayerDemoService/main.m
+++ RemoteLayerDemoService/main.m
@@ -16,7 +16,7 @@ static CARemoteLayerClient* sRemoteLayerClient = nil;
 static BOOL sIsTransactionOpen = NO;
 
 
-static void RemoteLayerDemoService_peer_event_handler(xpc_connection_t peer, xpc_object_t event) 
+static void RemoteLayerDemoService_peer_event_handler(xpc_connection_t peer, xpc_object_t event)
 {
 #if 0
     char* s = xpc_copy_description(event);
@@ -24,30 +24,30 @@ static void RemoteLayerDemoService_peer_event_handler(xpc_connection_t peer, xpc
     free(s);
 #endif
 
-	xpc_type_t type = xpc_get_type(event);
-	if (type == XPC_TYPE_ERROR) {
-		if (event == XPC_ERROR_CONNECTION_INVALID) {
-			// The client process on the other end of the connection has either
-			// crashed or cancelled the connection. After receiving this error,
-			// the connection is in an invalid state, and you do not need to
-			// call xpc_connection_cancel(). Just tear down any associated state
-			// here.
-		} else if (event == XPC_ERROR_TERMINATION_IMMINENT) {
-			// Handle per-connection termination cleanup.
-		}
-	} else {
-		assert(type == XPC_TYPE_DICTIONARY);
-		// Handle the message.
-        
+    xpc_type_t type = xpc_get_type(event);
+    if (type == XPC_TYPE_ERROR) {
+        if (event == XPC_ERROR_CONNECTION_INVALID) {
+            // The client process on the other end of the connection has either
+            // crashed or cancelled the connection. After receiving this error,
+            // the connection is in an invalid state, and you do not need to
+            // call xpc_connection_cancel(). Just tear down any associated state
+            // here.
+        } else if (event == XPC_ERROR_TERMINATION_IMMINENT) {
+            // Handle per-connection termination cleanup.
+        }
+    } else {
+        assert(type == XPC_TYPE_DICTIONARY);
+        // Handle the message.
+
         switch (xpc_dictionary_get_uint64(event, "command"))
         {
             case 0:
-                // We can't tell if the value was really 0, or if there was no value 
+                // We can't tell if the value was really 0, or if there was no value
                 // for this key in the dictionary. So:
             default:
                 // Do nothing.
                 break;
-                
+
             case 1:
             {
                 // Open a transaction, so the service support code doesn't
@@ -70,7 +70,7 @@ static void RemoteLayerDemoService_peer_event_handler(xpc_connection_t peer, xpc
 
                 break;
             }
-                
+
             case 2:
             {
                 // Change the color of the layer, in an animated way. No reply.
@@ -84,12 +84,12 @@ static void RemoteLayerDemoService_peer_event_handler(xpc_connection_t peer, xpc
                 CGColorRef c = CGColorCreateGenericRGB(r, g, b, 1.f);
                 sRemoteLayerClient.layer.backgroundColor = c;
                 CGColorRelease(c);
-                
+
                 [CATransaction commit];
 
                 break;
             }
-                
+
             case 3:
             {
                 // The app says we can stop now. No reply.
@@ -99,18 +99,18 @@ static void RemoteLayerDemoService_peer_event_handler(xpc_connection_t peer, xpc
                 }
                 break;
             }
-        }        
-	}
+        }
+    }
 }
 
-static void RemoteLayerDemoService_event_handler(xpc_connection_t peer) 
+static void RemoteLayerDemoService_event_handler(xpc_connection_t peer)
 {
-	// By default, new connections will target the default dispatch
-	// concurrent queue.
-	xpc_connection_set_event_handler(peer, ^(xpc_object_t event) {
-		RemoteLayerDemoService_peer_event_handler(peer, event);
-	});
-    
+    // By default, new connections will target the default dispatch
+    // concurrent queue.
+    xpc_connection_set_event_handler(peer, ^(xpc_object_t event) {
+        RemoteLayerDemoService_peer_event_handler(peer, event);
+    });
+
     // Look up the Mach port of the CARemoteLayerServer in the application.
     // Ideally the app would send us this port over XPC, but XPC can't send Mach ports...
     // So, for now, this is an insecure but straightforward way to get the port.
@@ -118,7 +118,7 @@ static void RemoteLayerDemoService_event_handler(xpc_connection_t peer)
     kern_return_t err = bootstrap_look_up(bootstrap_port, "com.snoize.RemoteLayerDemo.layerServerPort", &layerServerPort);
     if (err != KERN_SUCCESS) {
         NSLog(@"bootstrap_look_up failed: %d", err);
-    } else {        
+    } else {
         // Create a CARemoteLayerClient, and stuff a layer into it.
         sRemoteLayerClient = [[CARemoteLayerClient alloc] initWithServerPort:layerServerPort];
         if (!sRemoteLayerClient) {
@@ -128,26 +128,26 @@ static void RemoteLayerDemoService_event_handler(xpc_connection_t peer)
             if (!layer) {
                 layer = [CALayer layer];
                 layer.bounds = CGRectMake(0.f, 0.f, 100.f, 100.f);
-                
+
                 CGColorRef c = CGColorCreateGenericRGB(0.f, 1.f, 0.f, 1.f);
                 layer.backgroundColor = c;
                 CGColorRelease(c);
-                
+
                 sRemoteLayerClient.layer = layer;
             }
         }
     }
-    
+
     srand48(time(NULL));
-	
-	// This will tell the connection to begin listening for events. If you
-	// have some other initialization that must be done asynchronously, then
-	// you can defer this call until after that initialization is done.
-	xpc_connection_resume(peer);
+
+    // This will tell the connection to begin listening for events. If you
+    // have some other initialization that must be done asynchronously, then
+    // you can defer this call until after that initialization is done.
+    xpc_connection_resume(peer);
 }
 
 int main(int argc, const char *argv[])
 {
-	xpc_main(RemoteLayerDemoService_event_handler);
-	return 0;
+    xpc_main(RemoteLayerDemoService_event_handler);
+    return 0;
 }
